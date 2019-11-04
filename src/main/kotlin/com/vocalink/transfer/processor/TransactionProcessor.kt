1eeb4cd3cbd6eff34c2698d9c0e2f2e3d3f3339c
dursun gündoğan <>
2019-11-04 13:29:06 +0000

    init

--- src/main/kotlin/com/vocalink/transfer/processor/TransactionProcessor.kt
+++ src/main/kotlin/com/vocalink/transfer/processor/TransactionProcessor.kt
@@ -15,9 +15,6 @@ class TransactionProcessor {
         return CompletableFuture.supplyAsync {
             var result = Optional.empty<String>()
             when {
-                /*tx == null -> result = Optional.of("Transaction must not be null")
-                tx.id == null -> result = Optional.of("Transaction ID is required")
-                tx.amount == null -> result = Optional.of("Amount is required")*/
                 tx.amount.signum() <= 0 -> result = Optional . of ("Amount cannot be zero")
             }
             result
